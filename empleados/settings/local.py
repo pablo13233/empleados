79348b84afe74bb15af060b86c8ed5ba6bf859e5
Pablo Acosta <acosta13233@hotmail.com>
2022-04-07 00:59:01 -0600

    cambios

--- empleados/settings/local.py
+++ empleados/settings/local.py
@@ -11,8 +11,12 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'empleados',
+        'USER': 'postgres',
+        'PASSWORD': '!postgres!',
+        'HOST': '190.4.14.18',
+        'PORT': '5432',
     }
 }
 
