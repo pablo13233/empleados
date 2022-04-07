a867c2cb9fcb0c47c9e088cea57e967d9c62a1f1
Pablo Acosta <acosta13233@hotmail.com>
2022-04-06 22:17:24 -0600

    gitflow-hotfix-stash: as

--- applications/home/views.py
+++ applications/home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 # Create your views here.
 
 class PruebaView(TemplateView):
-    template_name = 'prueba.html'
+    template_name = 'home/prueba.html'
--- empleados/settings/base.py
+++ empleados/settings/base.py
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'empleados.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR.parent / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
