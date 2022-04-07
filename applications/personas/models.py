7c2ff855905f4518cc36458a84097f4a71700f15
Pablo Acosta <acosta13233@hotmail.com>
2022-04-07 00:28:20 -0600

    modelEmpleados

--- applications/personas/models.py
+++ applications/personas/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+from applications.departamentos.models import Departamento
+
 # Create your models here.
+class Empleado(models.Model):
+    """Modelo para tabla empleados"""
+    JOB_CHOISES = (
+        ('0', 'CONTADOR'),
+        ('1', 'ADMINISTRADOR'),
+        ('2', 'ECONOMISTA'),
+        ('3', 'OTRO'),
+    )
+    first_name = models.CharField('Nombres', max_length=60)
+    last_name = models.CharField('Apellidos', max_length=60)
+    job = models.CharField('Trabajo', max_length=1, choices=JOB_CHOISES)
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+    #image = models.ImageField('Imagen', upload_to=None, height_field=None, width_field=None, max_length=None)
+
+    def __str__(self):
+        return str(self.id) + ' - ' + self.first_name + ' ' + self.last_name
