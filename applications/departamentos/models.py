488b4ee482ab205832dd2adbdaf585e05a9172a3
Pablo Acosta <acosta13233@hotmail.com>
2022-04-07 00:14:08 -0600

    modeloDepartamentos

--- applications/departamentos/models.py
+++ applications/departamentos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Departamento(models.Model):
+    name = models.CharField('Nombre', max_length=50)
+    shor_name = models.CharField('Nombre Corto', max_length=20)
+    anulate = models.BooleanField('Anulado', default=False)
+
+    def __str__(self):
+        return str(self.id) + '-' + self.name + ' - ' + self.shor_name
